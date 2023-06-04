96a9e775cfec28b7be2f77c9f9f947c63a8c7e1b
moura <muradhani>
2023-06-04 17:52:29 +0300

    data binding setup

--- app/src/main/java/com/example/deltanews/MainActivity.kt
+++ app/src/main/java/com/example/deltanews/MainActivity.kt
@@ -2,20 +2,27 @@ package com.example.deltanews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
+import com.example.deltanews.databinding.ActivityMainBinding
 import com.google.firebase.FirebaseApp
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        FirebaseApp.initializeApp(this) // Initialize FirebaseApp
-
-        val database = FirebaseDatabase.getInstance()
-        val myref = database.getReference("message")
-        myref.setValue("hello world")
-        myref.get().addOnSuccessListener {
-            println(it.value)
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        binding.textOnScreen = "hellommm"
+//        binding.tv.postDelayed({
+//            binding.textOnScreen = "hello again!"
+//        },2_000)
+        val viewModel :NewsFeedViewModel = ViewModelProvider(this)[NewsFeedViewModel::class.java]
+        binding.lifecycleOwner = this
+        binding.viewmodel = viewModel
     }
 }
---
+++ app/src/main/java/com/example/deltanews/NewsFeedRepository.kt
@@ -0,0 +1,24 @@
+package com.example.deltanews
+
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class NewsFeedRepository {
+//    FirebaseApp.initializeApp(this) // Initialize FirebaseApp
+//
+//    val database = FirebaseDatabase.getInstance()
+//    val myref = database.getReference("message")
+//    myref.setValue("hello world")
+//    myref.addValueEventListener(object : ValueEventListener {
+//        override fun onDataChange(snapshot: DataSnapshot) {
+//            Log.i("SNAPSHOT",snapshot.value.toString())
+//        }
+//
+//        override fun onCancelled(error: DatabaseError) {
+//        }
+//
+//    })
+}
